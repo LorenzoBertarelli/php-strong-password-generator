ea1d181437f488fcd12314b0ad6538bfe4e7478d
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-05-05 19:07:38 +0200

    milestone 1

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
  $comb = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
- $pass = array(""); 
- $combLen = strlen($comb) - 1; 
- $password_length = $_GET["password"];
- for ($i = 0; $i = $password_length; $i++) {
+ $pass = array(); 
+ $combLen = strlen($comb) - 1;
+  
+ $password_length = isset($_GET["password"]) ? intval($_GET["password"]) : 0;
+ for ($i = 0; $i < $password_length; $i++) {
      $n = rand(0, $combLen);
-     $pass = $comb[$n];
+     array_push($pass, $comb[$n]);
  }
- print(implode($pass)); 
-?>
+ $password = implode('', $pass);
+ ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -21,11 +22,14 @@
 <body>
     <form action="index.php" method="GET">
         <label for="password">Inserisci il numero di caratteri con cui vuoi che venga composta la password</label> 
-        <input type="text" id="password" password_length="password">
+        <input type="text" id="password" name="password">
         <button type="submit">Genera password</button>
     </form>
-    <h2><?php echo $comb[$n] ?></h2>
-    <p><?php  ?></p>
+
+    <?php if (!empty($password)) { ?>
+        <h2>La password generata è:</h2>
+        <p><?php echo $password; ?></p>
+    <?php } ?>
 
 </body>
 </html>
