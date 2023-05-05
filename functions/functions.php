c28ac6b754040cb332acb951afaa408ef8896f15
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-05-05 20:08:06 +0200

    aggiunta session start

--- functions/functions.php
+++ functions/functions.php
@@ -1,4 +1,6 @@
 <?php
+ session_start();
+ $_SESSION['password'] = $password;
  $comb = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
  $pass = array(); 
  $combLen = strlen($comb) - 1;
--- includes/index.php
+++ includes/index.php
@@ -1,15 +1,19 @@
+<?php
+    session_start();
+    $password = $_SESSION['password'];
+    include __DIR__ . "/../functions/functions.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Generatore password</title>
 </head>
 <body>
-    <?php
-        include __DIR__ . "/../functions/functions.php"
-    ?>
+
     <form action="index.php" method="GET">
         <label for="password">Inserisci il numero di caratteri con cui vuoi che venga composta la password</label> 
         <input type="text" id="password" name="password">
@@ -19,6 +23,7 @@
     <?php if (!empty($password)) { ?>
         <h2>La password generata è:</h2>
         <p><?php echo $password; ?></p>
+        <p><a href="index.php">Genera una nuova password</a></p>
     <?php } ?>
 
 </body>
